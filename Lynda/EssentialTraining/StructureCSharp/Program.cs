def470ec2b181dd2dff1184755bd9debe133d2a5
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-05 16:23:35 +0000

    First look at C# code

---
+++ Lynda/EssentialTraining/StructureCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StructureCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int myVariable;
+
+			Console.WriteLine("Hello World");
+		}
+	}
+}
