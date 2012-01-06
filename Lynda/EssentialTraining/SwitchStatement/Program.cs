829ca5a9f15d55afbfd91dfbe0f24ffe088afab3
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-06 09:31:14 +0000

    Conditional statements - switch statements

---
+++ Lynda/EssentialTraining/SwitchStatement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwitchStatement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int theVal = 42;
+
+			switch (theVal)
+			{
+				case 50:
+					Console.WriteLine("The value is 50");
+					break;
+				case 51:
+					Console.WriteLine("The value is 51");
+					break;
+				case 52:
+					Console.WriteLine("The value is 52");
+					break;
+				default:
+					Console.WriteLine("The value is something else");
+					break;
+			}
+			Console.ReadLine();
+		}
+	}
+}
