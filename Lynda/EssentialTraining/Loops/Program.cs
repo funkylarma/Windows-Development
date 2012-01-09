b19f03a4546507f8f3fbca8ebe80368d4dcd967b
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-09 14:41:12 +0000

    Continue and break statements

--- Lynda/EssentialTraining/Loops/Program.cs
+++ Lynda/EssentialTraining/Loops/Program.cs
@@ -9,15 +9,24 @@ namespace Loops
 	{
 		static void Main(string[] args)
 		{
-			int myVal = 20;
+			//int myVal = 20;
 
-			//The for loop
-			Console.WriteLine("The for() loop:");
-			for (int i = 0; i < myVal; i += 5)
+			//Using the continue and break keywords
+			Console.WriteLine("Using break and continue");
+			for (int i = 0; i < 10; i++)
 			{
+				if (i == 5)
+				{
+					continue; //skip the rest of the loop
+				}
+
+				if (i == 9)
+				{
+					break;
+				}
+
 				Console.WriteLine("i is currently {0}", i);
 			}
-			Console.WriteLine();
 			
 			Console.ReadLine();
 		}
