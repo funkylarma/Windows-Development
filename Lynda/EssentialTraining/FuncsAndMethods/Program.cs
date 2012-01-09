16f19aee24b6a83f8b5a92528579105b72b71203
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-09 15:10:48 +0000

    Functions and methods

---
+++ Lynda/EssentialTraining/FuncsAndMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuncsAndMethods
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int result1;
+			int arg1 = 25;
+
+			result1 = formula(arg1);
+			Console.WriteLine("The result is: {0}", result1);
+			
+			Console.ReadLine();
+		}
+
+		static int formula(int theVal)
+		{
+			return (theVal * 2) / 3 + 15;
+		}
+
+	}
+}
