576cc27295f05dc280132ce1c82144ccf648fb16
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-09 13:14:05 +0000

    Constants and Wnums

---
+++ Lynda/EssentialTraining/ConstAndEnums/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstAndEnums
+{
+	class Program
+	{
+		enum Temperatures
+		{
+			FREEZING = 32,
+			LUKEWARM = 65,
+			ROOMTEMP = 72,
+			HOT = 105,
+			BOILING = 212
+		}
+		
+		static void Main(string[] args)
+		{
+
+			int myTemp = 65;
+
+			if (myTemp > (int)Temperatures.FREEZING && myTemp < (int)Temperatures.BOILING)
+			{
+				Console.WriteLine("At this temperature, water is a liquid");
+			}
+			else
+			{
+				Console.WriteLine("Water is not a liquid at this temperature");
+			}
+			
+			Console.ReadLine();
+		}
+	}
+}
