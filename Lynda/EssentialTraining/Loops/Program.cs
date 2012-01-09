63bc29686369bb93e9cb13ec6389166034f92c11
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-09 14:35:06 +0000

    for loops

--- Lynda/EssentialTraining/Loops/Program.cs
+++ Lynda/EssentialTraining/Loops/Program.cs
@@ -9,15 +9,14 @@ namespace Loops
 	{
 		static void Main(string[] args)
 		{
-			int myVal = 15;
+			int myVal = 20;
 
-			//The do-while loop
-			Console.WriteLine("The do-while() loop:");
-			do
+			//The for loop
+			Console.WriteLine("The for() loop:");
+			for (int i = 0; i < myVal; i += 5)
 			{
-				Console.WriteLine("myVal is currently {0}", myVal);
-				myVal += 3;
-			} while (myVal < 20);
+				Console.WriteLine("i is currently {0}", i);
+			}
 			Console.WriteLine();
 			
 			Console.ReadLine();
