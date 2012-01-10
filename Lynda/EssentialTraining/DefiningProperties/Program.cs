8fabd89a844da3c9e4382c5dfefe5197c9c972cf
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-10 15:15:41 +0000

    Class properties

---
+++ Lynda/EssentialTraining/DefiningProperties/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningProperties
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create some new Wine objects
+			Wine w1 = new Wine(2003, "Chateau Ste. Michelle Merlot", "Seven Hills", 23.50m);
+			Wine w2 = new Wine(2005, "Mark Ryan Dissident", "Ciel du Cheval", 40.00m);
+
+			//Write out the property values
+			//Note that in these cases we are using the 
+			Console.WriteLine("Wine 1: {0}, {1}", w1.MenuDescription, w1.Price);
+			Console.WriteLine("Wine 2: {0}, {1}", w2.MenuDescription, w2.Price);
+			Console.WriteLine();
+
+			//Change the wholesale price of one of the wines using the setter
+			w2.Price = 45.0m;
+
+			//Write out the wine description, note how the retail price automatically changes
+			Console.WriteLine("Wine 2: {0}, {1}", w2.MenuDescription, w2.Price);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Lynda/EssentialTraining/DefiningProperties/Wine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningProperties
+{
+	class Wine
+	{
+		//Class variables
+		private string Name;
+		private int year;
+		private string Apellation;
+		private decimal wholesalePrice;
+		private const decimal retailMarkup = 1.35m;
+
+		//Price property
+		public decimal Price
+		{
+			get
+			{
+				return wholesalePrice * retailMarkup;
+			}
+
+			set
+			{
+				wholesalePrice = value;
+			}
+		}
+
+		//MenuDescription property
+		public string MenuDescription
+		{
+			//Only a getter for this property, which is generated from private fields
+			get
+			{
+				return year.ToString() + " " + Name + " " + Apellation;
+			}
+		}
+
+		//Constructor method
+		public Wine(int y, string sName, string sApp, decimal wp)
+		{
+			Name = sName;
+			year = y;
+			Apellation = sApp;
+			wholesalePrice = wp;
+		}
+	}
+}
