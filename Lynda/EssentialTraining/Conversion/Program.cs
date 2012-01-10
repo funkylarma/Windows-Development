1c46052c3be6e686e930b54046314852d3e13442
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-10 13:38:44 +0000

    Type conversions and casting

---
+++ Lynda/EssentialTraining/Conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conversion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 800000;
+			short x = 5;
+			float f = 20.0f;
+
+			//i = x;
+
+			x = (short)i;
+			Console.WriteLine("The value of x is {0}", x);
+
+			f = i;
+
+			i = (int)f;
+
+			Console.ReadLine();
+		}
+	}
+}
