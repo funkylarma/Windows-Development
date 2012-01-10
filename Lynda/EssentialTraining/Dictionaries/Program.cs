3c1323cc7d17e8deebd497182a295aa849929fa3
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-10 16:59:50 +0000

    Collections - Dictionary/Hashtables

---
+++ Lynda/EssentialTraining/Dictionaries/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Dictionaries
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Hashtable myHT = new Hashtable();
+
+			myHT.Add("SFO", "San Francisco Aitport");
+			myHT.Add("SEA", "Seattle Tacoma Airport");
+			myHT["IAD"] = "Washington Dulles Aitport";
+
+			Console.WriteLine("Value for key {0} is {1}", "SEA", myHT["SEA"]);
+			Console.WriteLine("There are {0} items", myHT.Count);
+
+			myHT.Remove("SFO");
+			if (myHT.ContainsKey("SFO"))
+			{
+				Console.WriteLine("Value for key {0} is {1}","SFO", myHT["SFO"]);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
