0e3cd9423329dbb19162c35b8ea6709ca12a9ccf
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-11 16:00:44 +0000

    Overriding methods

---
+++ Lynda/EssentialTraining/MethodOverriding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodOverriding
+{
+	class baseClass
+	{
+		public virtual void doSomething()
+		{
+			Console.WriteLine("This is the baseClass saying Hi!");
+		}
+	}
+
+	class subClass : baseClass
+	{
+		public override void doSomething()
+		{
+			base.doSomething();
+			Console.WriteLine("This is the subClass saying Hi!");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			subClass obj1 = new subClass();
+
+			obj1.doSomething();
+
+			Console.ReadLine();
+		}
+	}
+}
