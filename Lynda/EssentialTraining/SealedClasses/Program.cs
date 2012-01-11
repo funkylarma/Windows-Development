4fd634c5ab360f3c0af13a02e6e7c0f08bf9c2e6
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-11 16:25:36 +0000

    Sealed classes

---
+++ Lynda/EssentialTraining/SealedClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SealedClasses
+{
+	sealed class myExampleClass
+	{
+		public static string myMethos(int arg1)
+		{
+			return String.Format("You sent me the number {0}", arg1);
+		}
+	}
+
+	class mySubClass : myExampleClass
+	{
+
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ReadLine();
+		}
+	}
+}
