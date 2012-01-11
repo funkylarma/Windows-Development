acae74e9030ecca052a84a71451bb4a8c57a77c8
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-11 17:11:10 +0000

    Introduction to exceptions

---
+++ Lynda/EssentialTraining/UsingExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingExceptions
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 10;
+			int y = 0;
+			int result;
+
+			try
+			{
+				result = x / y;
+				Console.WriteLine("The result is: {0}", result);
+			}
+
+			catch
+			{
+				Console.WriteLine("An error occured! Better check the code!");
+			}
+
+			finally
+			{
+				Console.WriteLine("Just procing that this code always runs.");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
