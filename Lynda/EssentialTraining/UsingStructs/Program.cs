0562d33d53e43d832a15b869a77fb7a0f25250fb
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-11 16:33:57 +0000

    Using structs

---
+++ Lynda/EssentialTraining/UsingStructs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UsingStructs
+{
+	public struct Point
+	{
+		private int xCoord;
+		private int yCoord;
+
+		public Point(int x, int y)
+		{
+			xCoord = x;
+			yCoord = y;
+		}
+
+		public int x
+		{
+			get { return xCoord; }
+			set { xCoord = value; }
+		}
+
+		public int y
+		{
+			get { return yCoord; }
+			set { yCoord = value; }
+		}
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Point p1 = new Point(100, 100);
+
+			Point p2 = new Point();
+			p2.x = 50;
+			p2.y = 75;
+
+			Console.ReadLine();
+		}
+	}
+}
