90980c19f3b6f8587faeb8424e9ecf18a9441327
Adam Chamberlin <adam.chamberlin@strident.co.uk>
2012-01-12 10:17:10 +0000

    The exception object

---
+++ Lynda/EssentialTraining/ExceptionObject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExceptionObject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int x = 10;
+			int y = 0;
+			int result;
+
+			try
+			{
+				result = x / y;
+				Console.WriteLine("The result is: {0}", result);
+			}
+			catch (System.ArithmeticException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Just proving that this code always runs.");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
